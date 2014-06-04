d03bbea57d1173b37b0a635eb1430d848295dbe7
Jake Nations <jnations1214@gmail.com>
2014-06-03 22:55:28 -0500

    game spec added to git

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,23 @@
 require "spec_helper"
+
+describe "Game" do
+
+  before :all do
+    @game = Game.new
+  end
+
+  describe "#initialize" do
+    it "initializes with a human player" do
+      @game.player.should be_an_instance_of Player
+    end
+    it "initializes with a computer player" do
+      @game.computer.should be_an_instance_of Computer
+    end
+    it "initializes with a current player" do
+      @game.current_player.should eq(@game.player)
+    end
+    it "initializes with a new board" do
+      @game.board.should be_an_instance_of Board
+    end
+  end
+end
